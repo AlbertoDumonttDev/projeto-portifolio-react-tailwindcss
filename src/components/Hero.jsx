b8c31131a77d5e8fce7916b9a4bbac670a060064
Alberto Dumontt <albertodumonttdev@outlook.com>
2024-12-22 00:09:19 -0300

    Feat: Add page

---
+++ src/components/Hero.jsx
@@ -0,0 +1,89 @@
+import { HERO_CONTENT } from '../constants';
+import profilePic from "../assets/albertoDumontt.png";
+import { motion } from "framer-motion";
+
+const container = (delay) => ({ 
+    hidden: { 
+        x: -100, 
+        opacity: 0 
+    },
+    visible: { 
+        x: 0,
+        opacity: 1,
+        transition: { 
+            duration: 0.5,
+            delay: delay 
+        }
+    },
+});
+
+const Hero = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+        <div className="flex flex-wrap">
+            <div className="w-full lg:w-1/2">
+                <div className="flex flex-col item-center lg:items-start">
+                    <motion.h1 
+                        variants={container(0)}
+                        initial="hidden"
+                        animate="visible"
+                        className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+                            Alberto Dumontt
+                    </motion.h1>
+                    <motion.span
+                        variants={container(0.5)}
+                        initial="hidden"
+                        animate="visible"
+                        className="bg-gradient-to-r from-pink-200 visa-slate-300 to-purple-300 bg-clip-text text-3xl tracking-tight text-transparent">
+                            Software Engineer
+                    </motion.span>
+                    <motion.p
+                        variants={container(1)}
+                        initial="hidden"
+                        animate="visible"
+                        className="my-2 max-w-xl py-6 font-light tracking-tight">
+                            {HERO_CONTENT}
+                    </motion.p>
+                    <div>
+                        <motion.a
+                            variants={container(1.5)}
+                            initial="hidden"
+                            animate="visible"
+                            href="https://docs.google.com/document/d/1Cm8YaSKRHYX-6OfM9SfDkXyc-TMcws-PGP-G0NHAphQ/edit?usp=sharing"
+                            target="_blank" 
+                            rel="noreferrer"
+                            className="rounded-2xl border-2 border-neutral-800 p-4 mt-4 mr-4 inline-block bg-gradient-to-r from-pink-200 via-slate-300 to-purple-300 bg-clip-text text-transparent hover:bg-gradient-to-r hover:from-pink-200 hover:via-slate-300 hover:to-purple-300 hover:text-white">
+                                CV (en)
+                        </motion.a>
+                        <motion.a
+                            variants={container(1.5)}
+                            initial="hidden"
+                            animate="visible"
+                            href="https://docs.google.com/document/d/1msTjLLRqI2u9M-mA9oRXHlHWEukm_xJaw8BIrJVUyoo/edit?usp=sharing"
+                            target="_blank" 
+                            rel="noreferrer"
+                            className="rounded-2xl border-2 border-neutral-800 p-4 mt-4 inline-block bg-gradient-to-r from-pink-200 via-slate-300 to-purple-300 bg-clip-text text-transparent hover:bg-gradient-to-r hover:from-pink-200 hover:via-slate-300 hover:to-purple-300 hover:text-white">
+                                CV (pt-br)
+                        </motion.a>
+                    </div>
+                </div>
+            </div>
+            <div className="w-full lg:w-1/2 lg:p-8">
+                <div className="flex justify-center">
+                    <motion.img
+                        initial={{ x: 100, opacity: 0 }}
+                        animate={{ x: 0, opacity: 1 }}
+                        transition={{ duration: 1, delay: 1.2 }}
+                        src={profilePic} width={500}
+                        className="rounded-2xl"
+                        height={500}
+                        alt="Alberto Dumontt"
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/constants/index.js
@@ -0,0 +1,58 @@
+import project1 from "../assets/projects/project-1.jpg";
+
+export const HERO_CONTENT = `I am a dedicated software engineer focused on developing complete and efficient applications. I have experience with front-end technologies such as Vue.js and React, as well as back-end technologies like Node.js, PHP, Python, and both relational and non-relational databases. My focus is on creating innovative and scalable solutions that meet user needs and drive business results.`;
+
+export const ABOUT_TEXT = `I am a software engineer with a degree in Computer Science, passionate about developing efficient and scalable applications. My experience spans various roles and technologies, enabling me to deliver impactful solutions for diverse challenges.
+
+Throughout my career, I have continuously honed my expertise in both front-end and back-end development, cloud services, database management, and IT infrastructure. My focus has always been on delivering value through innovative solutions and fostering collaboration to overcome complex challenges and drive results. I thrive in environments that allow me to solve complex problems while constantly learning and adapting to new technologies. In my free time, I enjoy going to the gym, running street races, and cheering for my football team, São Paulo FC. I also love playing video games and spending quality time with my family, balancing both personal and professional life to stay energized and motivated.`;
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Junior Software Engineer",
+    company: "E-Trust",
+    description: `I contribute to the maintenance and development of new features in the Horacius identity management system, using technologies such as Vue.js, jQuery, JavaScript, PHP, MySQL, SVN and AWS (IAM). Implementation of agile Kanban methodologies in JIRA, optimizing team efficiency. I perform code tests and reviews, improving code quality and promoting good development practices.
+Collaborate in the creation of solutions that increase the usability of the system, positively impacting the user experience. Participate in identifying bugs and resolving problems, contributing to system stability. I take a customer-focused approach, ensuring that new features meet the needs of end users. Improve internal processes through documenting practices and exchanging knowledge with the team, resulting in a collaborative and productive environment.`,
+    technologies: ["PHP", "Javascript", "JQuery", "Vue.js", "MySQL", "SVN"],
+  },
+  {
+    year: "2023 - 2024",
+    role: "IT Manager",
+    company: "Cartório de Registro de Imóveis de Cabreúva",
+    description: `I worked as a IT Manager at the Cabreúva Property Registry Office, assuming various responsibilities in the IT infrastructure and data management. I created and managed Linux servers, implementing and maintaining essential systems for the notary's operations. My experience with AWS was remarkable, involving the configuration and maintenance of services such as AWS Bedrock, AWS IAM, AWS S3 and AWS EC2. Developed Python scripts to automate processes and integrate third-party APIs, resulting in a significant improvement in operational efficiency and data security.
+Additionally, I explored the Google Cloud platform, using services like Google Storage to store crucial registry data. This work included the configuration and management of cloud resources, contributing to the modernization of infrastructure and optimization of internal processes. I also worked on the implementation and standardization of the registry regarding the LGPD, actively contributing to the creation and documentation of the necessary standards to ensure compliance and data protection. I also highlight my experience with Artificial Intelligence, where I worked with platforms such as OpenAI, Gemini and Claude. I developed skills in prompt engineering, creating models and integrating AI APIs, optimizing automated processes and offering innovative solutions for data management and operations.`,
+    technologies: ["Python", "AWS", "Google cloud", "Linux", "AI"],
+  },
+  {
+    year: "2023 - 2023",
+    role: "IT Support",
+    company: "Open Internet",
+    description: `I worked as IT support, performing a variety of functions related to infrastructure and technical support. Configured and implemented monitoring using Zabbix 6, ensuring the integrity and performance of the systems. Developed scripts in Bash and Batch to automate routine tasks, increasing the area's operational efficiency. I was responsible for hardware maintenance, carrying out diagnostics and repairs on computers and peripheral devices. Installed and configured software, ensuring that systems were always up to date and functioning correctly.
+I took care of the installation and configuration of printers, ensuring their integration with network systems. Led remote access, providing technical support to internal and external users whenever necessary. My experience covers both Windows environments and Linux servers, where I performed administration and maintenance tasks. Furthermore, I have skills in manipulating MySQL and PostgreSQL databases, contributing to effective data management.`,
+    technologies: ["Linux", "Windows", "Zabbix", "PowerShell", "Grafana", "MySQL", "Postgres", "Active Directory", "Looker Studios"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Software Development Intern",
+    company: "Open Dev",
+    description: `I worked as a development intern at OpenDev, getting involved in software projects focused on creating innovative solutions to optimize processes. A significant part of my work was dedicated to the development of REST APIs using PHP/Laravel, essential for system integration and process automation, contributing to operational efficiency. I was responsible for integrating an API for sending messages via WhatsApp and developing a chatbot, expanding communication channels with customers.
+In data management, I worked with MySQL and PostgreSQL databases, performing manipulation and optimization tasks to ensure fast and reliable access to information. In addition to developing APIs and integrations, I had the opportunity to create interactive reports with tools such as Grafana and Looker Studio, providing valuable insights into customer data and enabling more informed and strategic decision-making.`,
+    technologies: ["PHP", "Laravel", "MySQL", "PostGreSQL", "Grafana", "Looker Studios"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Raccoon Technology Management System",
+    image: project1,
+    ref: "https://github.com/RaccoonTechnologyManagement/RaccoonTechnologyManagement",
+    description:
+      "A comprehensive solution to meet the needs of IT departments, developed for capstone project. The system offers features such as: IT Asset Management, Call Management, Notices, Business Intelligence and Monitoring.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "Express", "PostgreSQL"],
+  }
+];
+
+export const CONTACT = {
+  address: "Cabreúva, São Paulo, Brazil ",
+  phoneNo: "+55 (11) 970578775 ",
+  email: "albertodumonttdev@outlook.com",
+};
